9cb46b64600574b10a3c38bb4c3fa9edf64456a3
leosakay <35719552+leosakay@users.noreply.github.com>
2024-05-12 06:13:24 +0000

    dbt project

--- demo_dbt_project/models/customers.sql
+++ demo_dbt_project/models/customers.sql
@@ -1,10 +1,12 @@
 -- data in the `coffee_shop` dataset¹ comes from our sales system
+{{ config(materialized='table') }}
+
+
 select customer_id, name, email, min(created_at) as first_order_at, count(*) as  number_of_orders
  from `analytics-engineers-club.coffee_shop.customers` as c
 inner join `analytics-engineers-club.coffee_shop.orders` as o
 on c.id = o.customer_id
 group by o.customer_id, name, email
 order by min(created_at)
-limit 5
-;
+
  
---
+++ demo_dbt_project/models/monthly_new_customers.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='view') }}
+
+select
+    date_trunc(first_order_at, month) as signup_month,
+    count(*) as new_customers
+
+from {{ ref('customers') }}
+group by 1
